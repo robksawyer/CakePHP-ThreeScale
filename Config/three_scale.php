cab1f7b2f5f81abf9a056e4183fffc2769afc482
Rob Sawyer <robksawyer@gmail.com>
2013-04-19 23:30:54 -0700

    Added config

---
+++ Config/three_scale.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Get a key and secret from AWS and fill in this content.
+*/
+	$config = array(
+		'ThreeScale' => array(
+			'provider_key' => CC_THREE_SCALE_PROVIDER_KEY,
+			'app_id' => CC_S3_THREE_SCALE_APP_ID,
+			'app_key' => CC_S3_THREE_SCALE_APP_KEY
+		)
+	);
+?>
