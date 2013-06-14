c042adc97181abd821e4f593f6c0d1301227f266
Rob Sawyer <robksawyer@gmail.com>
2013-06-14 07:23:00 -0700

    Added bootstrap sample

---
+++ Config/bootstrap.php.sample.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * API MANAGEMENT 3SCALE
+ * Instructions: Be sure to run ThreeScale => array('bootstrap'=>true) on load of the Plugin
+ * APP_ID = CLIENT_ID
+ * APP_KEY = CLIENT_SECRET
+ */
+define("THREE_SCALE_PROVIDER_KEY","SUPER_PRIVATE_PROVIDER_KEY");
+define("THREE_SCALE_APP_ID","TEST_APP_ID");
+define("THREE_SCALE_APP_KEY","TEST_APP_KEY");
+?>
