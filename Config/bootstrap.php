af0edbe64bb90f6008c6ae19ed7782fbb50d0a46
Rob Sawyer <robksawyer@gmail.com>
2013-06-20 17:02:04 -0700

    Fixed bug in config

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -6,13 +6,11 @@
  * app_key = client_secret
  */
 $threescale = array(
-	'Threescale' => array(
-		'provider_key' => getenv('THREE_SCALE_PROVIDER_KEY'),
-		'app_id' => getenv('THREE_SCALE_APP_ID'),
-		'app_key' => getenv('THREE_SCALE_APP_KEY'),
-		'endpoint' => getenv('THREE_SCALE_ENDPOINT'),
-		'account_id' => getenv('THREE_SCALE_ACCOUNT_ID')
-	)
+	'provider_key' => getenv('THREE_SCALE_PROVIDER_KEY'),
+	'app_id' => getenv('THREE_SCALE_APP_ID'),
+	'app_key' => getenv('THREE_SCALE_APP_KEY'),
+	'endpoint' => getenv('THREE_SCALE_ENDPOINT'),
+	'account_id' => getenv('THREE_SCALE_ACCOUNT_ID')
 );
 
 Configure::write('Threescale', $threescale);
